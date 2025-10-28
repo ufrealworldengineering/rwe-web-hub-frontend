152094b23292c11ae9b4908bf8f1a8216c150eb8
Zayd Tawam <zt44794@gmail.com>
2025-10-28 15:23:01 -0400

    Add about and navbar component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,40 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from "react-router-dom";
 
 const Layout = () => {
-    return (
-        <>
-            <main>
-                <Outlet />
-            </main>
-        </>
-    );
-}
+  const navigate = useNavigate();
+  return (
+    <>
+      <main>
+        <Outlet />
+      </main>
+      <div className="fixed bottom-4 left-1/2 -translate-x-1/2 z-50 bg-card border-1 rounded-full px-8 py-4 min-h-16 flex gap-8">
+        <button
+          className="text-lg font-semibold text-text-primary hover:text-accent"
+          onClick={() => navigate("/")}
+        >
+          Home
+        </button>
+        <button
+          className="text-lg font-semibold text-text-primary hover:text-accent"
+          onClick={() => navigate("/about")}
+        >
+          About
+        </button>
+        <button
+          className="text-lg font-semibold text-text-primary hover:text-accent"
+          onClick={() => navigate("/sponsors")}
+        >
+          Apply
+        </button>
+        <button
+          className="text-lg font-semibold text-text-primary hover:text-accent"
+          onClick={() => navigate("/applications")}
+        >
+          Sponsors
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Layout;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,100 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useState } from "react";
+
 const About = () => {
-    return (
-        <section className="space-y-4">
-            <h1 className="text-3xl font-serif text-text-primary">About</h1>
-            <p className="max-w-2xl text-foreground-secondary">
-                About...
-            </p>
-        </section>
-    );
-}
+  const teams = [
+    {
+      name: "General",
+      description: "Description.....",
+      members: [
+        { name: "First Last", role: "role", about: "about me info here..." },
+        { name: "First Last", role: "role", about: "about me info here..." },
+        { name: "First Last", role: "role", about: "about me info here..." },
+      ],
+    },
+    {
+      name: "Web Development",
+      description: "Description.....",
+      members: [
+        { name: "First Last", role: "role", about: "about me info here..." },
+        { name: "First Last", role: "role", about: "about me info here..." },
+        { name: "First Last", role: "role", about: "about me info here..." },
+      ],
+    },
+    {
+      name: "E-Bike",
+      description: "Description.....",
+      members: [
+        { name: "First Last", role: "role", about: "about me info here..." },
+        { name: "First Last", role: "role", about: "about me info here..." },
+        { name: "First Last", role: "role", about: "about me info here..." },
+      ],
+    },
+    {
+      name: "Robot Arm",
+      description: "Description.....",
+      members: [
+        { name: "First Last", role: "role", about: "about me info here..." },
+        { name: "First Last", role: "role", about: "about me info here..." },
+        { name: "First Last", role: "role", about: "about me info here..." },
+      ],
+    },
+  ];
+  const [currTeam, setCurrTeam] = useState(teams[0]);
+
+  return (
+    <section className="space-y-4 p-16">
+      <div className="flex flex-col md:flex-row items-center md:gap-16 gap-8 mb-16">
+        <h1 className="text-6xl font-black font-serif text-text-primary">
+          ABOUT
+        </h1>
+        <p className="text-3xl font-black font-serif text-text-secondary text-center">
+          mission statement
+        </p>
+      </div>
+      <Card className="flex mx-auto flex-col w-min md:w-full md:flex-row justify-center gap-20 mb-16 p-4">
+        {teams.map((team) => (
+          <button
+            key={team.name}
+            className={`flex-1 max-w-2x ${
+              currTeam.name === team.name
+                ? "font-bold text-text-primary"
+                : "font-normal text-text-secondary"
+            }`}
+            onClick={() => setCurrTeam(team)}
+          >
+            {team.name}
+          </button>
+        ))}
+      </Card>
+      <p className="mb-16">{currTeam.description}</p>
+      <div className="flex justify-center mb-16">
+        <p className="text-3xl font-black text-text-secondary">
+          Meet Our Executive Board
+        </p>
+      </div>
+      <div className="flex flex-col items-center lg:flex-row lg:justify-center gap-16">
+        {currTeam.members.map((member) => (
+          <Card className="min-w-56 max-w-100" key={member.name}>
+            <CardHeader>
+              <CardTitle>{member.name}</CardTitle>
+              <CardDescription>{member.role}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p className="mb-8">{member.about}</p>
+              <div className="aspect-square bg-black"></div>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default About;
