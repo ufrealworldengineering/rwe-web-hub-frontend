1eaff583deb49c30c49c2f6eb9047886caffb82e
Alina Garib <128430276+alinagarib@users.noreply.github.com>
2026-02-17 13:50:21 -0500

    Revert "Reformat single-page style"

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,38 @@
-import { Outlet, useNavigate } from 'react-router-dom';
-import Navbar from './navbar';
+import { Outlet, useNavigate } from "react-router-dom";
 
 const Layout = () => {
+  const navigate = useNavigate();
   return (
     <>
       <main>
         <Outlet />
-        {/* <Navbar /> */ }
       </main>
+      <div className="fixed bottom-4 left-1/2 -translate-x-1/2 z-50 bg-card border-1 rounded-full px-8 py-4 min-h-16 flex gap-8">
+        <button
+          className="text-lg font-semibold text-text-primary hover:text-accent"
+          onClick={() => navigate("/")}
+        >
+          Home
+        </button>
+        <button
+          className="text-lg font-semibold text-text-primary hover:text-accent"
+          onClick={() => navigate("/about")}
+        >
+          About
+        </button>
+        <button
+          className="text-lg font-semibold text-text-primary hover:text-accent"
+          onClick={() => navigate("/applications")}
+        >
+          Apply
+        </button>
+        <button
+          className="text-lg font-semibold text-text-primary hover:text-accent"
+          onClick={() => navigate("/sponsors")}
+        >
+          Sponsors
+        </button>
+      </div>
     </>
   );
 };
