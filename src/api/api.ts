cc1047029696582cd7267bd313e7218f662330e7
Noah Whelden <69485858+flarfo@users.noreply.github.com>
2026-03-09 16:03:54 -0400

    Change project structure (api folder)

--- src/api/api.ts
+++ src/api/api.ts
@@ -10,7 +10,7 @@ import { useAuthStore } from './store/authStore';
 interface ApiError {}
 
 const api: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
   headers: {
     'Content-Type': 'application/json',
   },
