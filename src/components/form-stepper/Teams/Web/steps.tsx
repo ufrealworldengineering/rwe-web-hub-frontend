3f0de40afcb8c6d77f7b937f6ad280ae7346adec
Noah Whelden <69485858+flarfo@users.noreply.github.com>
2026-03-09 16:08:36 -0400

    Update steps.tsx

--- src/components/form-stepper/Teams/Web/steps.tsx
+++ src/components/form-stepper/Teams/Web/steps.tsx
@@ -1,13 +1,5 @@
 import { useFormContext, Controller } from 'react-hook-form';
-import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from '@/components/ui/select';
 import {
     RadioGroup,
     RadioGroupItem,
