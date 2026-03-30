5e3008222e2d79306c1f3916744bbbe9d01be4a6
Noah Whelden <69485858+flarfo@users.noreply.github.com>
2026-03-30 11:32:06 -0400

    Import App component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './globals.css'
 import { ThemeProvider } from './lib/theme-context';
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
