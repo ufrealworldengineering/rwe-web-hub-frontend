b88f743b79dbb44ff235f530e5dc62496d3f28cb
Xeno <xeno.l20700@gmail.com>
2026-04-01 11:10:57 -0400

    Sidepart and Topbar implementation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,23 @@
 import { Outlet } from 'react-router-dom';
+import Sidepart from './Sidepart';
+import Topbar from './Topbar';
+
+
 // import Navbar from './navbar';
 
 const Layout = () => {
   return (
-    <>
-      <main>
-        <Outlet />
-        {/* <Navbar /> */ }
-      </main>
-    </>
+    <div className="flex h-screen w-full overflow-hidden bg-slate-50 dark:bg-slate-950 transition-colors duration-300">
+      <Sidepart />
+
+      <div className="flex-1 flex flex-col min-w-0 h-full">
+        <Topbar />
+        
+        <main className="flex-1 overflow-auto p-6 md:p-10">
+          <Outlet />
+        </main>
+      </div>
+    </div>
   );
 };
 
